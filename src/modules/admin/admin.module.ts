555b52d7eef1067aa62100f8a85b135537767dc6
Maphorbs <oluwadamilare.alonge@gmail.com>
2025-12-02 13:00:28 +0100

    Update admin.module.ts

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { OrderModule } from '../order/order.module';
+import { MenuModule } from '../menu/menu.module'; // Add this import
+import { FileStorageModule } from '../file-storage/file-storage.module'; // Add this import
 import { Vendor } from '@/entities';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Vendor]), OrderModule],
+  imports: [
+    TypeOrmModule.forFeature([Vendor]),
+    OrderModule,
+    MenuModule,           // Add this - exports CategoryService
+    FileStorageModule,    // Add this - exports FileStorageService
+  ],
   controllers: [AdminController],
   providers: [AdminService],
 })
